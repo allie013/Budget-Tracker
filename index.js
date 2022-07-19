bbff0f71ce7003f0b256df748ce2aebba2611078
Allie <allie@Allies-MacBook-Pro.local>
2022-07-18 21:20:11 -0700

    added questions to budget tracker

--- index.js
+++ index.js
@@ -2,4 +2,31 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
+//Included Arrarys
+//weeklyExpenseQuestions
+//monthylExpenseQuestions
+//annualExpenseQuestions
+
 // Your Code Here
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]));
+    console.log(answer);
+    weeklyExpenses = weeklyExpenses + answer;
+    console.log(weeklyExpenses)
+}
+
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]));
+    console.log(answer);
+    monthlyExpenses = monthlyExpenses + answer;
+    console.log(monthlyExpenses)
+}
+
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[i]));
+    console.log(answer);
+    annualExpenses = annualExpenses + answer;
+    console.log(annualExpenses)
+}
+
